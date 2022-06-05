d0fa1ae784b09321217c391b11c8803c0eed4266
atishayjain1x <atishayjain1x@gmail.com>
2022-06-06 01:07:19 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+ const express= require('express')
+const app=express()
+const path=require('path')
+const PORT=process.env.PORT || 3300
+const ejs=require('ejs')
+// const expressLayout=require('express-ejs-layoutsl')
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+// app.use(expressLayout)
+app.set('views',path.join(__dirname,'resources/views'))
+app.set('view engine','ejs')
+app.listen(3300,()=>{
+     console.log(`Listening on port ${PORT}`)
+ })

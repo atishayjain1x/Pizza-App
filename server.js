ab7f37f5f1a2a63f34dc06c3afb53138efcb2060
atishayjain1x <atishayjain1x@gmail.com>
2022-06-20 18:51:34 +0530

    auth

--- server.js
+++ server.js
@@ -1,28 +1,62 @@
- const express= require('express')
+require('dotenv').config()
+const express= require('express')
 const app=express()
 const path=require('path')
+const flash=require('express-flash')
 const PORT=process.env.PORT || 3300
 const ejs=require('ejs')
+const mongoose= require('mongoose')
+const session=require('express-session')
+const MongoDbStore = require('connect-mongo')(session)
 const expressLayout=require('express-ejs-layouts')
-app.use(expressLayout)
+const passport = require('passport')
 
+const url='mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).on('err',(err) => {
+    console.log('Connection failed...')
+});
+
+// Session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    store:mongoStore,
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24}
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+
+app.use(flash())
+
+app.use((req,res,next)=>{
+res.locals.session=req.session 
+res.locals.user=req.user
+next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(expressLayout)
 app.use(express.static('public'))
 
 app.set('views',path.join(__dirname,'resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-app.get('/cart',(req,res)=>{
-    res.render('customer/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+require('./routes/web')(app)
 app.listen(3300,()=>{
      console.log(`Listening on port ${PORT}`)
  })
